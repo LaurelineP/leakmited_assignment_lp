a28d07ce78fa253642858111edfbf5a860eb9480
Laureline <laurelinedev@gmail.com>
2024-08-02 16:18:18 +0200

    [ feature/roads-filtering ]

--- src/lib/utils/map.utils.ts
+++ src/lib/utils/map.utils.ts
@@ -2,38 +2,39 @@ import mapboxgl, { type FilterSpecification, type GeoJSONFeature, type MapOption
 export const layerId = 'roads';
 export const sourceId = 'IDFSource';
 
+export const filterRoads = (map: mapboxgl.Map, filters: [string, string, number | string][] = []) => {
+	if (!filters.length) return;
+	const infos: Record<string, string | number | null> = {
+		count: null
+	}
 
-
-export const addLayer = (map: mapboxgl.Map, SECRET_LAYERS_ID: string, filters: string[] = []) => {
-	const sourceFeatures = map.querySourceFeatures(sourceId, {
-		sourceLayer: SECRET_LAYERS_ID
-	});
-
-	const filteredFeatures: Array<GeoJSONFeature> = []
-
-
-	map.addSource(sourceId, {
-		type: 'geojson',
-		data: {
-			type: 'FeatureCollection',
-			features: filteredFeatures
+	filters?.forEach(filter => {
+		const [operator, field, value] = filter;
+		let _operator;
+		switch (operator) {
+			case 'sup':
+				_operator = '>=';
+				break;
+			case 'inf':
+				_operator = '<=';
+				break;
+			case 'eq':
+				_operator = '==';
+				break;
+			default:
+				_operator = '';
+				break;
 		}
-	});
+		if (filter.length === 3 && _operator) {
+			map.setFilter(layerId, [_operator, ['get', field], value] as FilterSpecification)
+			const remaining = map.queryRenderedFeatures();
 
-	map.addLayer({
-		'id': layerId,
-		'type': 'line', // Change to the appropriate type (e.g., 'fill', 'circle', etc.)
-		'source': sourceId,
-		'layout': {},
-		'paint': {
-			'line-color': '#fa0',
-			'line-width': 1
+			// info details
+			infos.count = remaining.length;
 		}
-	});
-}
+	})
 
-export const filterRoads = (map: mapboxgl.Map, filters: string[] = []) => {
-	map.setFilter(layerId, ['>=', ['get', 'maxspeed'], 1] as FilterSpecification)
+	return infos;
 }
 
 
@@ -56,46 +57,52 @@ export const addRoadsLayer = (map: mapboxgl.Map, SECRET_LAYER_ID: string) => {
 		},
 	);
 
-	// Filter right away to road with maxspeed 
-	const features = map.querySourceFeatures(sourceId, {
-		sourceLayer: SECRET_LAYER_ID
-	});
 
-	const maxSpeedFeatures = features.filter((feature: GeoJSONFeature) => {
-		return !!feature?.properties?.['maxspeed'];
-	});
-
-	filterRoads(map)
-
+	const infos = filterRoads(map, [['sup', 'maxspeed', 1]]);
 }
 
 /** Click handler to get info */
 export const onFeatureClick = (e) => {
+	const map = e.target;
 	const roadsFeatures = e.features;
 
 	function getFeaturesHTML(features: GeoJSONFeature[]) {
-		let strrr = features.map(f => {
+		const liElementsCollection = features.map(f => {
 			const featureStr = [];
 			if (f.properties) {
-				let lis = '';
+				let liElements = [];
+				let listTitle = ''
 				const propertiesEntries = Object.entries(f.properties);
+
+
+				// handles items  - li elements and title
 				for (let i = 0; i < propertiesEntries.length; i++) {
-					lis += `<li>${propertiesEntries[i][0]}: ${propertiesEntries[i][1]}</li>`;
-					if (i === propertiesEntries.length - 1) {
-						featureStr.push(lis);
-						lis = ''
+					const [field, value] = propertiesEntries[i];
+					if (field === 'name') {
+						listTitle = `<h1>${value}</h1>`;
+					} else {
+
+						// adds item element
+						liElements.push(`<li>${field}: ${value}`);
+
+						// handle the end of properties traversal
+						if (i === propertiesEntries.length - 1) {
+
+							liElements.unshift(`<div class="border p-2"><h1 class="flex justify-center">${listTitle}</h1><ul class="p-2">`)
+							liElements.push(`</div></ul>`)
+							featureStr.push(liElements.join(''));
+
+							liElements = [];
+						}
 					}
 				}
 			}
 			return featureStr.join('');
 		})
-		return strrr
+		return liElementsCollection.join('');
 	}
-
-	const itemsStr = getFeaturesHTML(roadsFeatures)
-
-
-	const content = `<ul>${itemsStr}</ul>`
+	const listElementsStr = getFeaturesHTML(roadsFeatures);
+	const content = `<div class="p-2 overflow-y-auto h-[300px] w-15 space-y-6">${listElementsStr}</div>`;
 
 	new mapboxgl.Popup()
 		.setLngLat(e.lngLat)
